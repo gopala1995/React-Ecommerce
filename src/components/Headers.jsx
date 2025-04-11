bfc607ed7f122e6fc10ffcf583cc38bc80115fcb
Gopala <ggopala1904@gmail.com>
2025-04-11 10:18:35 +0530

    chnages in taglines

---
+++ src/components/Headers.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router";
+
+const Headers = () => {
+  const { carts } = useSelector((state) => state.allCart);
+
+  return (
+    <>
+      <Navbar style={{ height: "60px", background: "teal", color: "white" }}>
+        <Container>
+          <NavLink to="/" className="text-decoration-none text-light mx-2">
+            <h3 className="text-light">Swimato</h3>
+          </NavLink>
+          <NavLink to="/cart" className="text-decoration-none text-light mx-2">
+            <div id="ex4">
+              <span
+                className="p1 fa-stack fa-2x has-badge"
+                data-count={carts.length}
+              >
+                <i className="fa-solid fa-cart-shopping"></i>
+              </span>
+            </div>
+          </NavLink>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Headers;
